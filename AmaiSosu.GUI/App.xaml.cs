e79082528167cb81519272c5f87274dd646d17ff
Emilian Roman <roman.emilian@outlook.com>
2018-11-16 17:32:29 +0800

    Initialise repository

---
+++ AmaiSosu.GUI/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AmaiSosu.GUI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ AmaiSosu.GUI/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace AmaiSosu.GUI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
